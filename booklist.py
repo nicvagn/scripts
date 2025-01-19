ae140f36335975e62c734407e9c654ad04c7a0f1
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-01-19 15:03:57 -0600

    added html format dir scrip

---
+++ booklist.py
@@ -0,0 +1,8 @@
+#!/usr/env python
+f = open("booklist.txt", "r")
+books = []
+
+for line in f:
+    books.append(line)
+
+breakpoint()
---
+++ format_html.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Format all the html in current dir with tidey
+
+html_files=`ls ./*.html`
+
+
+for f in $(ls ./*.html)
+do
+  echo $f
+  tidy -i -ashtml -utf8  -o ./"{$f}tidy"
+done
