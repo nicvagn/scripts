f3a99af67fcfee5cc5f8822b850271a594587402
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-18 08:15:28 -0600

    made more verbouse

--- run_fishnet.sh
+++ run_fishnet.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "ran: systemctl --user start fishnet"
 systemctl --user start fishnet
 bash /home/nrv/scripts/nosleep.sh
 systemctl --user stop fishnet
+echo "systemctl --user stop fishnet"
